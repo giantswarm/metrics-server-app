0758f4b98df590f76a069116204b9efd1f83c7df
Ross Fairbanks <ross@rossfairbanks.com>
2020-08-14 11:55:05 +0200

    Add missing helm label

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -117,8 +117,9 @@ func init() {
 						Name:      app,
 						Namespace: metav1.NamespaceSystem,
 						DeploymentLabels: map[string]string{
-							"giantswarm.io/service-type": "managed",
-							"app":                        app,
+							"app":                          app,
+							"app.kubernetes.io/managed-by": "Helm",
+							"giantswarm.io/service-type":   "managed",
 						},
 						MatchLabels: map[string]string{
 							"app": app,
